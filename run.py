824d69e5c7ebf7496afa77545eba567319a0553a
samuel <samuel.bonfim1234@gmail.com>
2025-01-13 22:29:37 -0300

    Ajusta Render

--- run.py
+++ run.py
@@ -1,13 +1,7 @@
-import sys
 import os
-from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).parent / "app"))
-
-from webapp import create_app  
+from webapp import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+# Remova o if __name__ == '__main__': pois o Gunicorn não o usa
+port = int(os.environ.get('PORT', 10000))
