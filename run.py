170ac761a540c3f138a415ef92daac8d295fb162
samuel <samuel.bonfim1234@gmail.com>
2025-01-19 09:56:09 -0300

    ajuste de portas de saida

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import os
 from webapp import create_app
 
+# Criando a aplicação Flask
 app = create_app()
 
-port = int(os.environ.get('PORT', 10000))
+if __name__ == "__main__":
+    # Porta definida pelo ambiente ou padrão 10000
+    port = int(os.environ.get('PORT', 10000))
+    # Inicia o servidor Flask, ouvindo em todas as interfaces de rede
+    app.run(host="0.0.0.0", port=port)
